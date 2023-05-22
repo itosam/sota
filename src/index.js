7c671b34464cfc9d0263d650f63a4aece7ec68dc
Osamu Ito <mr.samito@gmail.com>
2023-05-22 16:22:16 -0400

    Tech Assesment for SO inc. created webpage with assets and background image requested created form with background color change upon submit added styling and css to best match figma files received worked on mobile view for responsiveness

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("form");
+const body = document.body;
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const handle = document.querySelector("#handle");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //prevent form submit
+  console.log("form submitted");
+
+  body.classList.add("color-background");
+
+  name.value = "";
+  email.value = "";
+  handle.value = "";
+
+  alert("We have received your info! Thanks for signing up")
+});
